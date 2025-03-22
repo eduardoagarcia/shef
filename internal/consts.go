80e6b4a0834f008c52ff895bc4f60a5b3d63c53e
Eduardo A. Garcia <24eg14@gmail.com>
2025-03-22 09:54:22 -0500

    chore(feature-shef-v2): version bump and minor cleanup

--- internal/consts.go
+++ internal/consts.go
@@ -1,7 +1,7 @@
 package internal
 
 const (
-	Version               = "v0.1.15"
+	Version               = "v0.2.0"
 	GithubRepo            = "https://github.com/eduardoagarcia/shef"
 	PublicRecipesFilename = "recipes.tar.gz"
 	PublicRecipesFolder   = "recipes"
--- main.go
+++ main.go
@@ -1,28 +1,10 @@
 package main
 
 import (
-	//"bytes"
-	//"encoding/json"
-	//"fmt"
-	//"log"
-	//"math"
 	"math/rand"
-	//"os"
-	//"os/exec"
-	//"os/signal"
-	//"path/filepath"
-	//"sort"
-	//"strconv"
-	//"strings"
-	//"sync"
-	//"text/template"
 	"time"
 
-	//"github.com/AlecAivazis/survey/v2"
-	//"github.com/agnivade/levenshtein"
 	"github.com/eduardoagarcia/shef/internal"
-	//"github.com/urfave/cli/v2"
-	//"gopkg.in/yaml.v3"
 )
 
 func init() {
