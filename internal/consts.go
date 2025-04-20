5b0bfcf049aa89cd4f9880056a3255906dca840c
Eduardo A. Garcia <24eg14@gmail.com>
2025-04-20 14:58:28 -0500

    feature(fix-logic-bug): add file encrypted check

--- internal/consts.go
+++ internal/consts.go
@@ -6,5 +6,5 @@ const (
 	GithubRepo            = "https://github.com/eduardoagarcia/shef"
 	PublicRecipesFilename = "recipes.tar.gz"
 	PublicRecipesFolder   = "recipes"
-	Version               = "v0.3.1"
+	Version               = "v0.3.2"
 )
